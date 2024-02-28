a4fce610ff521703c5686b3f3ddcf744f351f576
Lovely.Value <aid.l.v.esd@gmail.com>
2024-02-28 17:17:04 +0300

    Lesson 3: Правка названия функций

--- js/main.js
+++ js/main.js
@@ -4,35 +4,34 @@ window.addEventListener('DOMContentLoaded', () => {
    const tabsContent = document.querySelectorAll('.tabcontent');
    const tabsParent = document.querySelector('.tabheader__items');
 
-   function hideTabsContent() {
+   function hideTabContent() {
       tabsContent.forEach(item => {
          item.classList.add('hide');
          item.classList.remove('show', 'fade');
       });
-
+      
       tabs.forEach(item => {
          item.classList.remove('tabheader__item_active');
       });
    }
 
-   function showTabsContent(i = 0) {
+   function showTabContent(i = 0) {
       tabsContent[i].classList.add('show', 'fade');
       tabsContent[i].classList.remove('hide');
       tabs[i].classList.add('tabheader__item_active');
    }
 
-   hideTabsContent();
-   showTabsContent();
-
+   hideTabContent();
+   showTabContent();
 
    tabsParent.addEventListener('click', (event) => {
       const target = event.target;
 
-      if (target && target.classList.contains('tabheader__item')) {
+      if(target && target.classList.contains('tabheader__item')) {
          tabs.forEach((item, i) => {
             if (target == item) {
-               hideTabsContent();
-               showTabsContent(i);
+               hideTabContent();
+               showTabContent(i);
             }
          });
       }
