ead601e0dc209537adbd268862aee6ed146521d1
Lovely.Value <aid.l.v.esd@gmail.com>
2024-03-07 18:03:02 +0300

    Lesson 8: Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -40,6 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 	//Таймер начало
 	const deadline = '2024-04-11';
+   
 	function getTimeRemaining(endtime) {
 		const t = Date.parse(endtime) - Date.parse(new Date()),
 			days = Math.floor(t / (1000 * 60 * 60 * 24)),
@@ -56,21 +57,21 @@ window.addEventListener('DOMContentLoaded', () => {
 		};
 	}
 
-   function getZero(num) {
-      if (num >= 0 && num < 10) {
-         retun `0${num}`;
-      } else {
-         return num;
-      }
+function getZero(num) {
+   if (num >= 0 && num < 10) {
+      return `0${num}`;
+   } else {
+      return num;
    }
+}
 
 	function setClock(selector, endtime) {
 		const timer = document.querySelector(selector),
-			days = timer.querySelector('#days'),
-			hours = timer.querySelector('#hours'),
-			minutes = timer.querySelector('#minutes'),
-			seconds = timer.querySelector('#seconds'),
-			timeInterval = setInterval(updateClock, 1000);
+			   days = timer.querySelector('#days'),
+			   hours = timer.querySelector('#hours'),
+			   minutes = timer.querySelector('#minutes'),
+			   seconds = timer.querySelector('#seconds'),
+			   timeInterval = setInterval(updateClock, 1000);
 
 		updateClock();
 
