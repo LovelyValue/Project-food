1dfa66cbe325113f293b90a71366966f0a02bce5
Lovely.Value <aid.l.v.esd@gmail.com>
2024-03-11 17:29:01 +0300

    Lesson 9

--- js/main.js
+++ js/main.js
@@ -39,14 +39,23 @@ window.addEventListener('DOMContentLoaded', () => {
 	// Табы конец
 
 	//Таймер начало
-	const deadline = '2024-04-11';
-   
+	const deadline = '2024-03-11';
+
 	function getTimeRemaining(endtime) {
-		const t = Date.parse(endtime) - Date.parse(new Date()),
-			days = Math.floor(t / (1000 * 60 * 60 * 24)),
-			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-			minutes = Math.floor((t / 1000 / 60) % 60),
+		const t = Date.parse(endtime) - Date.parse(new Date());
+		let days, hours, minutes, seconds;
+
+		if (t <= 0) {
+			days = 0;
+			hours = 0;
+			minutes = 0;
+			seconds = 0;
+		} else {
+			days = Math.floor(t / (1000 * 60 * 60 * 24));
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			minutes = Math.floor((t / 1000 / 60) % 60);
 			seconds = Math.floor((t / 1000) % 60);
+		}
 
 		return {
 			total: t,
@@ -57,21 +66,21 @@ window.addEventListener('DOMContentLoaded', () => {
 		};
 	}
 
-function getZero(num) {
-   if (num >= 0 && num < 10) {
-      return `0${num}`;
-   } else {
-      return num;
-   }
-}
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return `0${num}`;
+		} else {
+			return num;
+		}
+	}
 
 	function setClock(selector, endtime) {
 		const timer = document.querySelector(selector),
-			   days = timer.querySelector('#days'),
-			   hours = timer.querySelector('#hours'),
-			   minutes = timer.querySelector('#minutes'),
-			   seconds = timer.querySelector('#seconds'),
-			   timeInterval = setInterval(updateClock, 1000);
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
 
 		updateClock();
 
