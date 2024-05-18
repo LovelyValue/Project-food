342222c214cbd36bf44bed39197430d355926538
Lovely.Value <aid.l.v.esd@gmail.com>
2024-05-18 19:11:36 +0300

    Lesson 10

--- js/main.js
+++ js/main.js
@@ -36,7 +36,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
-	// Табы конец
 
 	// Таймер начало
 	const deadline = '2024-03-11';
@@ -99,7 +98,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	setClock('.timer', deadline);
-	//Таймер конец
 
 	// Модальное окно начало
 	const modalTrigger = document.querySelectorAll('[data-modal'),
@@ -144,7 +142,6 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 
 	window.addEventListener('scroll', showModalByScroll);
-	// Модальное окно конец
 
 	// Использование классов ля карточек начало
 	class MenuCard {
@@ -188,38 +185,47 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	new MenuCard(
-		'img/tabs/vegy.jpg',
-		'vegy',
-		'Меню "Фитнес"',
-		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-		9,
-		'.menu .container',
-		'menu__item'
-	).render();
+	const getREsource = async url => {
+		const res = await fetch(url);
 
-	new MenuCard(
-		'img/tabs/post.jpg',
-		'post',
-		'Меню "Постное"',
-		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-		14,
-		'.menu .container',
-		'menu__item'
-	).render();
+		if (!res.ok) {
+			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+		}
 
-	new MenuCard(
-		'img/tabs/elite.jpg',
-		'elite',
-		'Меню “Премиум”',
-		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-		21,
-		'.menu .container'
-	).render();
-	//Использование классов для карточке конец
+		return await res.json();
+	};
+
+	getREsource('http://localhost:3000/menu').then(data => {
+		data.forEach(({ img, altimg, title, descr, price }) => {
+			new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+		});
+	});
+
+	// getREsource('http://localhost:3000/menu')
+	// .then(data => createCard(data));
+
+	// function createCard(data) {
+	// 	data.forEach(({ img, altimg, title, descr, price }) => {
+	// 		const element = document.createElement('div');
+
+	// 		element.classList.add('menu__item');
+
+	// 		element.innerHTML = `
+	// 		<img src=${img} alt=${altimg}/>
+	// 		<h3 class="menu__item-subtitle">${title}</h3>
+	// 		<div class="menu__item-descr">${descr}</div>
+	// 		<div class="menu__item-divider"></div>
+	// 		<div class="menu__item-price">
+	// 				<div class="menu__item-cost">Цена:</div>
+	// 				<div class="menu__item-total"><span>${price}</span> грн/день</div>
+	// 		</div>
+	// 		`;
+
+	// 		document.querySelector('.menu .container').append(element);
+	// 	});
+	// }
 
 	//Формы
-
 	const forms = document.querySelectorAll('form');
 
 	const message = {
@@ -229,10 +235,22 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	forms.forEach(item => {
-		postData(item);
+		bindPostData(item);
 	});
 
-	function postData(form) {
+	const postData = async (url, data) => {
+		const res = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/JSON',
+			},
+			body: data,
+		});
+
+		return await res.json();
+	};
+
+	function bindPostData(form) {
 		form.addEventListener('submit', e => {
 			e.preventDefault();
 
@@ -247,19 +265,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 			const formData = new FormData(form);
 
-			const object = {};
-			formData.forEach(function (value, key) {
-				object[key] = value;
-			});
+			const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-			fetch('server.php', {
-				method: 'POST',
-				headers: {
-					'Content-type': 'application/JSON',
-				},
-				body: JSON.stringify(object),
-			})
-				.then(data => data.text())
+			postData('http://localhost:3000/requests', json)
 				.then(data => {
 					console.log(data);
 					showThanksModal(message.success);
